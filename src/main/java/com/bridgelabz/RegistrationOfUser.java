825b96a13c0e823c8ce924d2f60f8f3728985689
mvinchankar <mvinchankar18@gmail.com>
2019-11-22 17:06:53 +0530

    ADDED test case for valid last names

--- src/main/java/com/bridgelabz/RegistrationOfUser.java
+++ src/main/java/com/bridgelabz/RegistrationOfUser.java
@@ -4,13 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegistrationOfUser {
-    final static Pattern firstNamePattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+    final static Pattern firstNamePattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
 
     public static boolean validate_First_Name(String firstName) {
         Matcher m = firstNamePattern.matcher(firstName);
         if (m.matches())
             return true;
-        else
-            return false;
+        return false;
+    }
+    public static boolean validate_Last_Name(String lastName) {
+        Matcher m = firstNamePattern.matcher(lastName);
+        if (m.matches())
+            return true;
+        return false;
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -21,6 +21,11 @@ public class UserRegistrationTest {
         Assert.assertFalse(registrationOfUser.validate_First_Name("123Mangesh"));
         Assert.assertFalse(registrationOfUser.validate_First_Name("MMangesh"));
         Assert.assertFalse(registrationOfUser.validate_First_Name("Om"));
+    } @Test
+    public void when_Given_Last_Name_Should_Return_True() {
+        Assert.assertTrue(registrationOfUser.validate_Last_Name("Rai"));
+        Assert.assertTrue(registrationOfUser.validate_Last_Name("Pau"));
+        Assert.assertTrue(registrationOfUser.validate_Last_Name("Rao"));
     }
 
 }
