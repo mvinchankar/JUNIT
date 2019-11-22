1ba3fb89d02ad385eedde087b849b7b92c87cfd2
mvinchankar <mvinchankar18@gmail.com>
2019-11-22 18:17:22 +0530

    ADDED test case for invalid mobile numbers

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -13,20 +13,24 @@ public class UserRegistrationTest {
         Assert.assertTrue(registrationOfUser.validate_First_Name("Priya"));
         Assert.assertTrue(registrationOfUser.validate_First_Name("Ravi"));
     }
+
     @Test
-    public void when_Given_First_Name_Should_Return_False(){
+    public void when_Given_First_Name_Should_Return_False() {
         Assert.assertFalse(registrationOfUser.validate_First_Name("M312angesh"));
         Assert.assertFalse(registrationOfUser.validate_First_Name("123Mangesh"));
         Assert.assertFalse(registrationOfUser.validate_First_Name("MMangesh"));
         Assert.assertFalse(registrationOfUser.validate_First_Name("Om"));
-    } @Test
+    }
+
+    @Test
     public void when_Given_Last_Name_Should_Return_True() {
         Assert.assertTrue(registrationOfUser.validate_Last_Name("Rai"));
         Assert.assertTrue(registrationOfUser.validate_Last_Name("Pau"));
         Assert.assertTrue(registrationOfUser.validate_Last_Name("Rao"));
     }
+
     @Test
-    public void when_Given_Last_Name_Should_Return_False(){
+    public void when_Given_Last_Name_Should_Return_False() {
         Assert.assertFalse(registrationOfUser.validate_Last_Name("h"));
         Assert.assertFalse(registrationOfUser.validate_Last_Name("1233223424"));
         Assert.assertFalse(registrationOfUser.validate_Last_Name("MasdaRao"));
@@ -37,6 +41,7 @@ public class UserRegistrationTest {
     public void when_Given_Mobile_Number_Should_Return_True() {
         Assert.assertTrue(registrationOfUser.validate_Mobile_Number("91 9552967330"));
     }
+
     @Test
     public void when_Given_Mobile_Number_Should_Return_False() {
         Assert.assertFalse(registrationOfUser.validate_Mobile_Number("552967330"));
